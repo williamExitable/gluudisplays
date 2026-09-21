00085768014a6eda823138f2c6929d7c11f3b0ff
William Meuwissen <william@exitable.nl>
2026-09-21 15:31:17 +0200

    Add custom banner slides

---
+++ assets/custom-banner.js
@@ -0,0 +1,48 @@
+class CustomBannerSlider extends HTMLElement {
+  constructor() {
+    super();
+    this.slides = Array.from(this.querySelectorAll('.custom-banner__slide'));
+    this.index = 0;
+    this.speed = parseInt(this.dataset.autoplaySpeed, 10) || 6000;
+    this.transitionDuration = 900;
+  }
+
+  connectedCallback() {
+    if (this.slides.length < 2) return;
+
+    this.observer = new IntersectionObserver((entries) => {
+      entries[0].isIntersecting ? this.play() : this.pause();
+    });
+    this.observer.observe(this);
+  }
+
+  disconnectedCallback() {
+    this.pause();
+    this.observer?.disconnect();
+  }
+
+  play() {
+    if (this.timer) return;
+    this.timer = setInterval(() => this.next(), this.speed);
+  }
+
+  pause() {
+    clearInterval(this.timer);
+    this.timer = null;
+  }
+
+  next() {
+    const current = this.slides[this.index];
+    this.index = (this.index + 1) % this.slides.length;
+    const upcoming = this.slides[this.index];
+
+    current.classList.replace('custom-banner__slide--active', 'custom-banner__slide--leaving');
+    current.setAttribute('aria-hidden', 'true');
+    upcoming.classList.add('custom-banner__slide--active');
+    upcoming.removeAttribute('aria-hidden');
+
+    setTimeout(() => current.classList.remove('custom-banner__slide--leaving'), this.transitionDuration);
+  }
+}
+
+customElements.define('custom-banner-slider', CustomBannerSlider);
